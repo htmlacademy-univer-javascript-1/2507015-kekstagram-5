46f98e8f4027db208c1e801e7e980e993f34f45e
Дарья Бочкарева <bochkarevadsgmail.com>
2024-11-10 17:59:28 +0500

    point

--- js/util.js
+++ js/util.js
@@ -1,3 +1,19 @@
-export function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+export const getRandomInt = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+export const getRandomArrayElement = (array) =>
+  array[getRandomInt(0, array.length - 1)];
+
+export const createIdGenerator = () => {
+  let lastGeneratedId = 0;
+
+  return () => {
+    lastGeneratedId += 1;
+    return lastGeneratedId;
+  };
+};
+
