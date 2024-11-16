e15bcb7dbfc7b75f7a18fddbaa77e568ac3895de
Дарья Бочкарева <bochkarevadsgmail.com>
2024-11-16 15:13:21 +0500

    notwork((

--- js/miniatureDrawing.js
+++ js/miniatureDrawing.js
@@ -1,20 +1,20 @@
 const pictureTemplate = document
   .querySelector('#picture')
   .content.querySelector('.picture');
-const picturesBox = document.querySelector('.pictures');
-const createPictureElement = ({ comments, description, likes, url }) => {
+
+const createPictureElement = ({ comments, description, likes, url, id }) => {
   const pictureElement = pictureTemplate.cloneNode(true);
 
   pictureElement.querySelector('.picture__img').src = url;
   pictureElement.querySelector('.picture__img').alt = description;
-  pictureElement.querySelector('.pictur__comments').textContent = comments.length;
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
   pictureElement.querySelector('.picture__likes').textContent = likes;
+  pictureElement.daraset.pictureId = id;
 
   return pictureElement;
 };
 
-
-const renderPictures = (pictures) => {
+const renderPictures = (pictures, picturesBox) => {
   const fragment = document.createDocumentFragment();
   pictures.forEach((picture) => {
     const pictureElement = createPictureElement(picture);
