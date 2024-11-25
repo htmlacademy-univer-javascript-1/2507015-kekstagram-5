819caaa02cb1fd72ea8892b2e0de84e738e3ac44
Keks <keks@htmlacademy.ru>
2024-11-25 11:19:59 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/util.js
@@ -0,0 +1,20 @@
+export const getRandomInt = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+export const getRandomArrayElement = (array) =>
+  array[getRandomInt(0, array.length - 1)];
+
+export const createIdGenerator = () => {
+  let lastGeneratedId = 0;
+
+  return () => {
+    lastGeneratedId += 1;
+    return lastGeneratedId;
+  };
+};
+
+export const isEscapeKey = (evt) => evt.key === 'Escape';
