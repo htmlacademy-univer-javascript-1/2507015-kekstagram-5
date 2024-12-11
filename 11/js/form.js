0616c2b2794b5fa1a47f39d913dc39f0f4db9d1e
Keks <keks@htmlacademy.ru>
2024-12-11 16:37:11 +0300

    :heavy_check_mark: Сборка #11

--- 11/js/form.js
+++ 11/js/form.js
@@ -1,7 +1,4 @@
 import {isEscapeKey} from './util.js';
-import { resetScale } from './size.js';
-import { init, reset } from './effects.js';
-
 const MAX_HASHTAG_COUNT = 5;
 const VALID_SYMBOLS = /^#[a-zа-яё0-9]{1,19}$/i;
 const ErrorText = {
@@ -9,7 +6,6 @@ const ErrorText = {
   NOT_UNIQUE: 'Хэштеги должны быть уникальными',
   INVALIDE_PATTERN: 'Неправильный хэштег'
 };
-
 const form = document.querySelector('.img-upload__form');
 const overlay = form.querySelector('.img-upload__overlay');
 const body = document.querySelector('body');
@@ -18,23 +14,18 @@ const pristine = new Pristine(form, {
   classTo: 'img-upload__field-wrapper',
   errorTextParent: 'img-upload__field-wrapper',
 });
-
 const showModal = () => {
   overlay.classList.remove('hidden');
   body.classList.add('modal-open');
   document.addEventListener('keydown', onDocumentKeydown);
 };
-
 const hideModal = () => {
   form.reset();
-  resetScale();
-  reset();
   pristine.reset();
   overlay.classList.add('hidden');
   body.classList.remove('modal-open');
   document.removeEventListener('keydown', onDocumentKeydown);
 };
-
 const normalizeTags = (tagString) => tagString.trim().split(' ').filter((tag) => Boolean(tag.length));
 const isTextFieldFocused = () => document.activeElement === hashtagField || document.activeElement === form.querySelector('.text__description');
 const hasValidCount = (value) => normalizeTags(value).length <= MAX_HASHTAG_COUNT;
@@ -43,14 +34,12 @@ const hasUnidueTags = (value) => {
   const lowerCaseTags = normalizeTags(value).map((tag) => tag.toLowerCase());
   return lowerCaseTags.length === new Set(lowerCaseTags).size;
 };
-
 function onDocumentKeydown(evt) {
   if (isEscapeKey && !isTextFieldFocused()) {
     evt.preventDefault();
     hideModal();
   }
 }
-
 const onCancelButtonClick = () => hideModal();
 const onFileInputChange = () => showModal();
 pristine.addValidator(
@@ -60,7 +49,6 @@ pristine.addValidator(
   3,
   true
 );
-
 pristine.addValidator(
   hashtagField,
   hasValidTags,
@@ -68,7 +56,6 @@ pristine.addValidator(
   2,
   true
 );
-
 pristine.addValidator(
   hashtagField,
   hasUnidueTags,
@@ -76,7 +63,6 @@ pristine.addValidator(
   1,
   true
 );
-
 form.querySelector('.img-upload__input').addEventListener('change', onFileInputChange);
 form.querySelector('.img-upload__cancel').addEventListener('click', onCancelButtonClick);
-init();
+
