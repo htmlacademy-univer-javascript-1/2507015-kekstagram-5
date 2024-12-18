796deb4aef35a55f571516bc7f195eb5d70c9cba
Дарья Бочкарева <bochkarevadsgmail.com>
2024-12-18 21:08:51 +0500

    11/

--- js/fetch.js
+++ js/fetch.js
@@ -1,29 +1,27 @@
-/*
-const Urls = {
-GET: 'https: //31.javascript.html academy.pro/kickstagram/data',
-POST: 'https://31.javascript.htmlacademy.pro/kekstagram',
+const BASE_URL = 'https://29.javascript.htmlacademy.pro/kekstagram';
+const Route = {
+  GET_DATA:'/data',
+  SEND_DATA: '/',
 };
-
-const sendRequest = (onSuccess, onError, method, body) => (
-fetch(
-UrlsImethod),
-
-body: body,
-method: method,
-
-then((response) => response, json())
-then((data) => (
-onSuccess(data);
-))
-•Catch((err)
-onError(err);
-»);
+const Method = {
+  GET: 'GET',
+  POST: 'POST',
 };
-
-const load Data = (onSuccess, onError, method = 'GET') => send Request(onSuccess, onError, method);
-
-const upload Data = (onSuccess, onError, method = 'POST', body) => send Request(onSuccess, onError, method, body);
-
-export (load Data, upload Data};
-2
-*/
+const ErrorText = {
+  GET_DATA: 'Не удалось загрузить данные. Попробуйте обновить страницу',
+  SEND_DATA: 'Не удалось отправить форму. Попробуйте еще раз',
+};
+const load = (route, errorText, method = Method.GET, body = null) =>
+  fetch(`${BASE_URL}${route}`, {method, body})
+    .then((response) => {
+      if (response.status !== 200) {
+        throw new Error();
+      }
+      return response.json();
+    })
+    .catch(() => {
+      throw new Error(errorText);
+    });
+const getData = () => load(Route.GET_DATA, ErrorText.GET_DATA);
+const sendData = (body) => load(Route.SEND_DATA, ErrorText.SEND_DATA, Method.POST, body);
+export{getData, sendData};
