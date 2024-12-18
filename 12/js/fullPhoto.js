ab066c7586a5b76523a84bdc47312a89c804ff19
Keks <keks@htmlacademy.ru>
2024-12-18 19:27:52 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/fullPhoto.js
@@ -0,0 +1,23 @@
+import { renderPictures } from './miniatureDrawing.js';
+import { showBigPicture } from './bigPhoto.js';
+
+const container = document.querySelector('.pictures');
+
+const renderGallery = (pictures) => {
+  container.addEventListener('click', (evt) => {
+    const thumbnail = evt.target.closest('[data-picture-id]');
+    if (!thumbnail) {
+      return;
+    }
+
+    evt.preventDefault();
+    const pic = pictures.find(
+      (item) => item.id === +thumbnail.dataset.pictureId
+    );
+    showBigPicture(pic);
+  });
+
+  renderPictures(pictures, container);
+};
+
+export {renderGallery};
