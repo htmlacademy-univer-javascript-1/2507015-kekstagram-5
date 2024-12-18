3504e7cd5b9a3e1843249281b3abad0ae66ea464
Дарья Бочкарева <bochkarevadsgmail.com>
2024-12-18 13:14:27 +0500

    11

---
+++ js/fetch.js
@@ -0,0 +1,29 @@
+/*
+const Urls = {
+GET: 'https: //31.javascript.html academy.pro/kickstagram/data',
+POST: 'https://31.javascript.htmlacademy.pro/kekstagram',
+};
+
+const sendRequest = (onSuccess, onError, method, body) => (
+fetch(
+UrlsImethod),
+
+body: body,
+method: method,
+
+then((response) => response, json())
+then((data) => (
+onSuccess(data);
+))
+•Catch((err)
+onError(err);
+»);
+};
+
+const load Data = (onSuccess, onError, method = 'GET') => send Request(onSuccess, onError, method);
+
+const upload Data = (onSuccess, onError, method = 'POST', body) => send Request(onSuccess, onError, method, body);
+
+export (load Data, upload Data};
+2
+*/
