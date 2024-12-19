9b20a47befa282931feed886ddd51aff13456a0c
Keks <keks@htmlacademy.ru>
2024-12-19 18:53:07 +0300

    :heavy_check_mark: Сборка #13

---
+++ 13/js/main.js
@@ -0,0 +1,22 @@
+import { renderGallery } from './fullPhoto.js';
+import { showSuccessMessage, showErrorMessage } from './data.js';
+import { hideModal, setOnFormSubmit } from './form.js';
+import { getData, sendData } from './fetch.js';
+import { showAlert, debounce } from './util.js';
+import { init, getFilterPictures } from './filter.js';
+
+try {
+  init(await getData(), debounce(renderGallery));
+  renderGallery(getFilterPictures());
+} catch (err) {
+  showAlert(err.message);
+}
+setOnFormSubmit(async (data) => {
+  try {
+    await sendData(data);
+    hideModal();
+    showSuccessMessage();
+  } catch {
+    showErrorMessage();
+  }
+});
